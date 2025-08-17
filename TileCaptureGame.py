85942a9b2f1544e4c6c46bb9df779d55ec04c7a0
amyn-13 <mynguyen.mjr@gmail.com>
2025-08-17 11:06:49 -0700

    Updated TileCapturGame check_capture

--- TileCaptureGame.py
+++ TileCaptureGame.py
@@ -204,13 +204,24 @@ class TileCaptureGame:
                    for col in range(BOARD_SIZE))
 
     def check_captures(self, row, col):
-        surrounding = self.get_adjacent_positions(row, col)
-        for sr, sc in surrounding:
-            if self.board[sr][sc] is not None and self.board[row][col] != self.current_player:
-                current_surrounding = self.count_adjacent_tile(sr, sc, self.current_player)
-                opponent_surrounding = self.count_adjacent_tile(sr, sc, self.board[sr][sc])
-                if current_surrounding > opponent_surrounding:
-                    self.board[sr][sc] = self.current_player
+        def check_captures(self, row, col):  # if making move at row col, then can I capture any tiles
+            surrounding_tile = self.get_adjacent_positions(row, col)
+            # print(f"surrounding: {surrounding_tile}")
+            for sr, sc in surrounding_tile:
+                if self.board[sr][sc] is not None and self.board[sr][sc] != self.current_player:
+                    # print(f"current player: {self.current_player}")
+                    # print(f"opponent: {self.board[sr][sc]}")
+                    opponent_surrounding = self.count_adjacent_tile(sr, sc, self.board[sr][sc])
+                    # print(f"Opponent:{opponent_surrounding}")
+                    current_surrounding = self.count_adjacent_tile(sr, sc, self.current_player)
+                    # print(f"Current:{current_surrounding}")
+                    if current_surrounding > 1:
+                        self.board[sr][sc] = self.current_player
+                        self.player1_moves.append((sr, sc))
+                        # self.player2_moves.remove((sr,sc))
+                        # self.update_player_valid_moves(self.current_player)
+                        # self.update_player_valid_moves(self.board[sr][sc])
+                        return
 
     def early_termination(self):
         if len(self.player1_valid_moves) == 0 or len(self.player2_valid_moves) == 0:
