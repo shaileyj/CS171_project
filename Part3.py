ee0f4ef5e4cb29daa3f58716285c6f02a8648448
shaileyj <shaileyjoseph2006@gmail.com>
2025-08-18 10:44:01 -0700

    Created initial minimax template for user to write

---
+++ Part3.py
@@ -0,0 +1,36 @@
+from minimax import generate_moves, simple_heuristic
+
+PLAYER_1 = 1
+PLAYER_2 = 2
+
+class GameTree:
+    def __init__(self, value, board, children=None):
+        self.value = value
+        self.board = board
+        self.children = []
+
+def build_tree(board, depth, player=PLAYER_1):
+    """Builds the minimax tree"""
+    #Assume player 1 is the max player and
+    #player 2 is the min player for simplicity
+
+    root = GameTree(None, board)
+    children = generate_moves(board, player)
+    if depth <=1 or len(children) == 0:
+        ...
+    for move in children:
+        ...
+    return root
+
+
+def minimax(tree, is_max):
+    """Runs the minimax algorithm"""
+    #Base case:
+    if len(tree.children) == 0:
+        ...
+    #Recursive cases:
+    if is_max:
+        ...
+    else:
+        ...
+    return tree.value
